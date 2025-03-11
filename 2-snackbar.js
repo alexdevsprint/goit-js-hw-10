1556775e713b88eb673e77d2bc51496ade709428
alexdevsprint <alexdevsprint@gmail.com>
2025-03-11 13:41:53 +0000

    Deploying to gh-pages from @ alexdevsprint/goit-js-hw-10@f607736aa6326f9e070581664d9b7546d30267a0 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const o=document.querySelector(".form");o.addEventListener("submit",t);function t(e){e.preventDefault(),console.log("hello")}
+//# sourceMappingURL=2-snackbar.js.map
