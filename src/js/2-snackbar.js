f607736aa6326f9e070581664d9b7546d30267a0
alexdevsprint <alexdevsprint@gmail.com>
2025-03-11 15:41:28 +0200

    commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,11 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const sumbmitForm = document.querySelector('.form');
+
+sumbmitForm.addEventListener('submit', createPromise);
+
+function createPromise(event) {
+  event.preventDefault();
+  console.log('hello');
+}
